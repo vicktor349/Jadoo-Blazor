1867e268fb4e34374cdab2bef6b8ef276234772f
vicktoh <ayomidevictor349@gmail.com>
2024-01-28 20:48:23 +0100

    Added telegram icon and fixed border radius

--- obj/Debug/net8.0/Jadoo.AssemblyInfo.cs
+++ obj/Debug/net8.0/Jadoo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Jadoo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b8d224dc94f77db50d1192b10b502063e00ab17")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff69445c53de525030ccd6b4ee20c35d31959f5a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Jadoo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Jadoo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
